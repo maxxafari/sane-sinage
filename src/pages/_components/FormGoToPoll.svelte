c2311a1755a3cefda99f5b0d25eb8e073b6f8a7d
Max Berkenstam <max.berkenstam@gmail.com>
2020-10-24 13:29:34 +0200

    add first component

---
+++ src/pages/_components/FormGoToPoll.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { goto } from '@sveltech/routify'
+  let pollId = null
+  const submit = (e) => {
+    $goto(`/poll/${pollId}`)
+  }
+  function focus(el) {
+    el.focus()
+  }
+</script>
+
+<form on:submit|preventDefault={submit}>
+  <input
+    id="pollNumber"
+    name="pollNumber"
+    type="text"
+    bind:value={pollId}
+    use:focus
+  />
+  <button type="submit" disabled={!pollId}>Join poll</button>
+</form>
